6a900709e041b5bc7219cead0e39f57c0b7d2abe
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-03-15 19:39:04 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -22,8 +22,8 @@ class Triangle
       end
     end
   end
-end
 
-class TriangleError < StandardError
-  # triangle error code
+  class TriangleError < StandardError
+    # triangle error code
+  end
 end
