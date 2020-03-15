a4b95070216ed8507dd1dfe41d1336b8b25966e5
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-03-15 18:57:01 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,6 +10,8 @@ class Triangle
       raise TriangleError
     elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
       raise TriangleError
+    elsif @side_1 && @side_2 && side_3 == 0
+      raise Illegal
     else
       if (@side_1 == @side_2) && (@side_2 == @side_3)
         :equilateral
@@ -17,8 +19,6 @@ class Triangle
         :isosceles
       elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
         :scalene
-      elsif @side_1 && @side_2 && @side_3 == 0
-        :illegal
       end
     end
   end
