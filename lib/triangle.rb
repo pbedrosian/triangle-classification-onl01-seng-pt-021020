8a5e251adfaa4cb93091cd4e7b0d35dc14d60be0
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-03-15 18:43:33 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,7 +17,7 @@ class Triangle
         :isosceles
       elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
         :scalene
-      elsif @side_1 && @side_2 && side_3 == 0
+      elsif @side_1 && @side_2 && @side_3 == 0
         :illegal
       end
     end
