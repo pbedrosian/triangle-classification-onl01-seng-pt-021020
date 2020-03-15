a38f91f2bf854b53a8cbd3303aece4a2628e4d03
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-03-15 19:39:48 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,8 +10,8 @@ class Triangle
       raise TriangleError
     elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
       raise TriangleError
-    elsif (@side_1 = 0) && (@side_2 = 0) && (@side_3 = 0)
-      raise TriangleError
+    # elsif (@side_1 = 0) && (@side_2 = 0) && (@side_3 = 0)
+    #   raise TriangleError
     else
       if (@side_1 == @side_2) && (@side_2 == @side_3)
         :equilateral
@@ -23,7 +23,7 @@ class Triangle
     end
   end
 
-  class TriangleError < StandardError
-    # triangle error code
-  end
+  # class TriangleError < StandardError
+  #   # triangle error code
+  # end
 end
